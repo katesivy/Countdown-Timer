45e22b54bc11ced858a2e835f357aaa65e35b46a
katesivy <58646769+katesivy@users.noreply.github.com>
2020-02-21 14:44:12 -0500

    commits

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 
-let now = new Date().getTime();
-let end = new Date("May 15, 2020 17:00:00").getTime();
-let timerAmount = end - now;
-
-/*function timerElements() {
-  let d = new Date();
-  let h = d.getHours();
-  let m = d.getMinutes();
-  let s = d.getSeconds();
-  return `${h} : ${m} : ${s}`;
-}
-Math.floor(timerAmount / (1000 * 60 * 60 * 24));
-*/
 
 function displayTimer() {
-    const y = '50';
+  let now = new Date().getTime();
+  let end = new Date("Feb 24, 2020 8:00:00").getTime();
+  let timerAmount = end - now;
+
+    var days = Math.floor(timerAmount / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(timerAmount % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var minutes = Math.floor(timerAmount % (1000 * 60 * 24) / (1000 * 60));
+    var seconds = Math.floor(timerAmount % (1000 * 60) / (1000));
+    
     const x = document.getElementById('timer');
-    x.textContent = y;
-}
+    if (timerAmount > 0) {
+    x.textContent = `Days ${days} : Hours ${hours} : Minutes ${minutes} : Seconds ${seconds}`;
+    } else x.textContent = '';
+  }
+
+displayTimer();
+setInterval(displayTimer, 1000);
